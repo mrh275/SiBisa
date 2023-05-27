3379bcdec36a152fc010b76c2cae573a36d53e53
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-05-27 10:55:25 +0700

    create api config for request to api

---
+++ app/src/main/java/com/mrh/sibisa/network/api/ApiConfig.kt
@@ -0,0 +1,33 @@
+package com.mrh.sibisa.network.api
+
+import android.content.Context
+import com.mrh.sibisa.BuildConfig
+import com.mrh.sibisa.network.util.AuthInterceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ApiConfig {
+
+    companion object {
+        fun getApiService(context: Context) : ApiService {
+            val token = "token"
+            val loggingInterceptor = if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            } else {
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+            }
+            val client = OkHttpClient.Builder()
+                .addInterceptor(AuthInterceptor("Bearer $token"))
+                .addInterceptor(loggingInterceptor)
+                .build()
+            val retrofit = Retrofit.Builder()
+                .baseUrl(BuildConfig.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+            return retrofit.create(ApiService::class.java)
+        }
+    }
+}
