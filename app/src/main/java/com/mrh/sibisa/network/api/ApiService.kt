e1718435e243b1a939d765fa283ad9fea41c9ae9
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-05-27 10:55:07 +0700

    create login request method

---
+++ app/src/main/java/com/mrh/sibisa/network/api/ApiService.kt
@@ -0,0 +1,16 @@
+package com.mrh.sibisa.network.api
+
+import com.mrh.sibisa.data.ResponseLogin
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
+interface ApiService {
+
+    @FormUrlEncoded
+    @POST("login")
+    suspend fun login(
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): ResponseLogin
+}
