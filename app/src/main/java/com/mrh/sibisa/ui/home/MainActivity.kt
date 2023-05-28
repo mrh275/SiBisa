e35c6d9559300d6e3dd4270c479ef70bf3ba9066
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-05-28 13:11:39 +0700

    create method to request permission and event method to get each navbar menu fragment

--- app/src/main/java/com/mrh/sibisa/ui/home/MainActivity.kt
+++ app/src/main/java/com/mrh/sibisa/ui/home/MainActivity.kt
@@ -1,16 +1,48 @@
 package com.mrh.sibisa.ui.home
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.commit
-import com.mrh.sibisa.HomeFragment
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.mrh.sibisa.ui.home.fragment.HomeFragment
 import com.mrh.sibisa.R
 import com.mrh.sibisa.databinding.ActivityMainBinding
+import com.mrh.sibisa.ui.auth.login.LoginActivity
+import com.mrh.sibisa.ui.home.fragment.ProfileFragment
+import com.mrh.sibisa.ui.home.fragment.SettingFragment
+import com.mrh.sibisa.ui.translate.TranslateActivity
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == REQUEST_CODE_PERMISSIONS) {
+            if(!allPermissionsGranted()) {
+                Toast.makeText(
+                    this,
+                    "Tidak mendapatkan permission.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -18,15 +50,49 @@ class MainActivity : AppCompatActivity() {
         binding.bottomNavigation.background = null
         binding.bottomNavigation.menu.getItem(2).isEnabled = false
 
-        val fragmentManager = supportFragmentManager
         val homeFragment = HomeFragment()
-        val fragment = fragmentManager.findFragmentByTag(HomeFragment::class.java.simpleName)
+        val profileFragment = ProfileFragment()
+        val settingFragment = SettingFragment()
 
-        if(fragment !is HomeFragment) {
-            Log.d("MyFlexibleFragment", "Fragment Name : " + HomeFragment::class.java.simpleName)
-            fragmentManager.commit {
-                add(R.id.frame_container, homeFragment, HomeFragment::class.java.simpleName)
-            }
+        if (!allPermissionsGranted()) {
+            ActivityCompat.requestPermissions(
+                this,
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
         }
+
+        replaceFragment(homeFragment)
+        binding.fabTranslate.setOnClickListener {
+            val intent = Intent(this, TranslateActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.nav_home -> replaceFragment(homeFragment)
+                R.id.nav_profile -> replaceFragment(profileFragment)
+                R.id.nav_setting -> replaceFragment(settingFragment)
+                R.id.nav_login -> {
+                    startActivity(Intent(this, LoginActivity::class.java))
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        fragmentManager.beginTransaction()
+            .setCustomAnimations(R.anim.slide_in_rtl, R.anim.fade_out)
+            .replace(R.id.frame_container, fragment)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+            .commit()
+    }
+    companion object {
+        const val CAMERA_X_RESULT = 200
+
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
     }
 }
