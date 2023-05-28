004c5cc7e892f1437c08bcc5dbf55ca75cd50f02
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-05-28 11:34:09 +0700

    added fragment to activity

--- app/src/main/java/com/mrh/sibisa/ui/home/MainActivity.kt
+++ app/src/main/java/com/mrh/sibisa/ui/home/MainActivity.kt
@@ -2,6 +2,10 @@ package com.mrh.sibisa.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.commit
+import com.mrh.sibisa.HomeFragment
+import com.mrh.sibisa.R
 import com.mrh.sibisa.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +17,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.bottomNavigation.background = null
         binding.bottomNavigation.menu.getItem(2).isEnabled = false
+
+        val fragmentManager = supportFragmentManager
+        val homeFragment = HomeFragment()
+        val fragment = fragmentManager.findFragmentByTag(HomeFragment::class.java.simpleName)
+
+        if(fragment !is HomeFragment) {
+            Log.d("MyFlexibleFragment", "Fragment Name : " + HomeFragment::class.java.simpleName)
+            fragmentManager.commit {
+                add(R.id.frame_container, homeFragment, HomeFragment::class.java.simpleName)
+            }
+        }
     }
 }
