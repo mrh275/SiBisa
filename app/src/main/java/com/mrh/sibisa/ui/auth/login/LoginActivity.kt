1787133035557014d7cbb828af8abc4c74456640
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-06-02 18:02:45 +0700

    added method auth firebase for login and register

--- app/src/main/java/com/mrh/sibisa/ui/auth/login/LoginActivity.kt
+++ app/src/main/java/com/mrh/sibisa/ui/auth/login/LoginActivity.kt
@@ -3,17 +3,22 @@ package com.mrh.sibisa.ui.auth.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.mrh.sibisa.databinding.ActivityLoginBinding
 import com.mrh.sibisa.ui.auth.register.RegisterActivity
 import com.mrh.sibisa.ui.home.MainActivity
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
+        firebaseAuth = FirebaseAuth.getInstance()
         setContentView(binding.root)
 
         binding.tvSignup.setOnClickListener {
@@ -21,7 +26,26 @@ class LoginActivity : AppCompatActivity() {
         }
 
         binding.btnLogin.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
+            try {
+                val email = binding.fieldEmail.text.toString()
+                val password = binding.fieldPassword.text.toString()
+                if(email.isNotEmpty() && password.isNotEmpty()) {
+                    firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                        if(it.isSuccessful) {
+                            startActivity(Intent(this, MainActivity::class.java))
+                            finish()
+                        } else {
+                            Log.e("LoginActivity", it.exception.toString())
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "Kolom tidak boleh kosong!", Toast.LENGTH_SHORT).show()
+                }
+            } catch (e: Exception) {
+                Log.e("LoginActivity", e.message.toString())
+                Toast.makeText(this, "Login gagal! Kendala server", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
--- app/src/main/java/com/mrh/sibisa/ui/auth/register/RegisterActivity.kt
+++ app/src/main/java/com/mrh/sibisa/ui/auth/register/RegisterActivity.kt
@@ -3,16 +3,22 @@ package com.mrh.sibisa.ui.auth.register
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.mrh.sibisa.databinding.ActivityRegisterBinding
 import com.mrh.sibisa.ui.auth.login.LoginActivity
+import com.mrh.sibisa.ui.home.MainActivity
 
 class RegisterActivity : AppCompatActivity() {
 
+    private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var binding: ActivityRegisterBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
+        firebaseAuth = FirebaseAuth.getInstance()
         setContentView(binding.root)
 
         binding.tvLogin.setOnClickListener {
@@ -20,7 +26,25 @@ class RegisterActivity : AppCompatActivity() {
         }
 
         binding.btnRegister.setOnClickListener {
-            startActivity(Intent(this, LoginActivity::class.java))
+            try {
+                val email = binding.fieldEmail.text.toString()
+                val password = binding.fieldPassword.text.toString()
+                if(email.isNotEmpty() && password.isNotEmpty()) {
+                    firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                        if(it.isSuccessful) {
+                            startActivity(Intent(this, LoginActivity::class.java))
+                            finish()
+                        } else {
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "Kolom tidak boleh kosong!", Toast.LENGTH_SHORT).show()
+                }
+            } catch (e: Exception) {
+                Log.e("LoginActivity", e.message.toString())
+                Toast.makeText(this, "Registrasi gagal! Kendala server", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
