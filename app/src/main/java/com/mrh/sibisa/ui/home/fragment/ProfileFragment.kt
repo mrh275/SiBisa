0c9b30d0919aeacefddee0f56aed5b04721aeac5
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-06-16 00:21:50 +0700

    modified profile layout value from shared preferences

--- app/src/main/java/com/mrh/sibisa/ui/home/fragment/ProfileFragment.kt
+++ app/src/main/java/com/mrh/sibisa/ui/home/fragment/ProfileFragment.kt
@@ -1,10 +1,12 @@
 package com.mrh.sibisa.ui.home.fragment
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.mrh.sibisa.R
 
 class ProfileFragment : Fragment() {
@@ -19,5 +21,15 @@ class ProfileFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val sharedPreferences = context?.getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
+
+        val profileName = view.findViewById<TextView>(R.id.tv_profile_name)
+        val profileEmail = view.findViewById<TextView>(R.id.tv_profile_email)
+        profileName.text = sharedPreferences?.getString("NAME", "Nama Pengguna")
+        profileEmail.text = sharedPreferences?.getString("EMAIL", "email@email.com")
     }
 }
