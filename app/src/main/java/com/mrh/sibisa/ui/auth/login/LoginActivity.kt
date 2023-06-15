26213eaf163ae7de828c40c357dbb70e27e943b3
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-06-15 17:05:53 +0700

    create method to login

--- app/src/main/java/com/mrh/sibisa/ui/auth/login/LoginActivity.kt
+++ app/src/main/java/com/mrh/sibisa/ui/auth/login/LoginActivity.kt
@@ -1,52 +1,68 @@
 package com.mrh.sibisa.ui.auth.login
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
-import com.google.firebase.auth.FirebaseAuth
 import com.mrh.sibisa.databinding.ActivityLoginBinding
+import com.mrh.sibisa.ui.auth.AuthViewModel
 import com.mrh.sibisa.ui.auth.register.RegisterActivity
 import com.mrh.sibisa.ui.home.MainActivity
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var viewModel: AuthViewModel
+    private var backPressedTime: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
-        firebaseAuth = FirebaseAuth.getInstance()
         setContentView(binding.root)
+        viewModel = AuthViewModel()
 
         binding.tvSignup.setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
         }
 
         binding.btnLogin.setOnClickListener {
+            showLoading(true)
+            val sharedPreferences = getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
+            val editorPref = sharedPreferences.edit()
             try {
                 val email = binding.fieldEmail.text.toString()
                 val password = binding.fieldPassword.text.toString()
-                if(email.isNotEmpty() && password.isNotEmpty()) {
-                    firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
-                        if(it.isSuccessful) {
-                            Toast.makeText(this, "Login berhasil! Selamat datang $email",Toast.LENGTH_SHORT).show()
-                            startActivity(Intent(this, MainActivity::class.java))
-                            finish()
-                        } else {
-                            Log.e("LoginActivity", it.exception.toString())
-                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
-                        }
-                    }
-                } else {
-                    Toast.makeText(this, "Kolom tidak boleh kosong!", Toast.LENGTH_SHORT).show()
+
+                viewModel.login(email, password)
+                viewModel.getAuthData().observe(this) {
+                    editorPref.apply {
+                        putString("AUTH_TOKEN", it?.accessToken)
+                    }.apply()
+
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finishAfterTransition()
+                    showLoading(false)
                 }
             } catch (e: Exception) {
                 Log.e("LoginActivity", e.message.toString())
                 Toast.makeText(this, "Login gagal! Kendala server", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+    override fun onBackPressed() {
+        if(backPressedTime + 3000 > System.currentTimeMillis()) {
+            super.onBackPressed()
+            finish()
+        } else {
+            Toast.makeText(this, "Press back again to exit", Toast.LENGTH_SHORT).show()
+        }
+        backPressedTime = System.currentTimeMillis()
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
     }
 }
