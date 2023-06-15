4c04c907f177ceadc3d7df380150c57e77921711
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-06-15 17:07:36 +0700

    added register and logout request

--- app/src/main/java/com/mrh/sibisa/network/api/ApiService.kt
+++ app/src/main/java/com/mrh/sibisa/network/api/ApiService.kt
@@ -1,6 +1,8 @@
 package com.mrh.sibisa.network.api
 
-import com.mrh.sibisa.data.ResponseLogin
+import com.mrh.sibisa.data.login.ResponseLogin
+import com.mrh.sibisa.data.logout.ResponseLogout
+import com.mrh.sibisa.data.register.ResponseRegister
 import com.mrh.sibisa.data.sign.ResponseSign
 import retrofit2.Call
 import retrofit2.http.Field
@@ -12,10 +14,21 @@ interface ApiService {
 
     @FormUrlEncoded
     @POST("login")
-    suspend fun login(
+    fun login(
         @Field("email") email: String,
         @Field("password") password: String
-    ): ResponseLogin
+    ): Call<ResponseLogin>
+
+    @POST("logout")
+    fun logout() : Call<ResponseLogout>
+
+    @FormUrlEncoded
+    @POST("register")
+    fun register(
+        @Field("name") name: String,
+        @Field("email") email: String,
+        @Field("password") password: String
+    ) : Call<ResponseRegister>
 
     @GET("signs")
     fun getAllSigns(): Call<ResponseSign>
