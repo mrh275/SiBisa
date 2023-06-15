3bb2d55a2b29ceae1e79fcb8b20585cadc04d37b
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-06-16 00:21:06 +0700

    modified request method for news

--- app/src/main/java/com/mrh/sibisa/ui/home/MainViewModel.kt
+++ app/src/main/java/com/mrh/sibisa/ui/home/MainViewModel.kt
@@ -4,8 +4,8 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.mrh.sibisa.data.sign.ResponseSign
-import com.mrh.sibisa.data.sign.SignItem
+import com.mrh.sibisa.data.news.NewsDataItem
+import com.mrh.sibisa.data.news.ResponseNews
 import com.mrh.sibisa.network.api.ApiConfig
 import retrofit2.Call
 import retrofit2.Callback
@@ -13,28 +13,25 @@ import retrofit2.Response
 
 class MainViewModel : ViewModel() {
 
-    val listSigns = MutableLiveData<List<SignItem?>?>()
+    val listNews = MutableLiveData<List<NewsDataItem>>()
 
     fun setSigns() {
-        val client = ApiConfig.getApiService().getAllSigns()
-        client.enqueue(object : Callback<ResponseSign> {
-            override fun onResponse(call: Call<ResponseSign>, response: Response<ResponseSign>) {
-                if(response.isSuccessful) {
+        val client = ApiConfig.getApiService().getAllNews()
+        client.enqueue(object : Callback<ResponseNews> {
+            override fun onResponse(call: Call<ResponseNews>, response: Response<ResponseNews>) {
+                if (response.isSuccessful) {
                     val responseBody = response.body()
-                    if(responseBody != null) {
-                        listSigns.postValue(responseBody.data)
-                    }
+                    listNews.postValue(responseBody?.newsData)
                 }
             }
 
-            override fun onFailure(call: Call<ResponseSign>, t: Throwable) {
+            override fun onFailure(call: Call<ResponseNews>, t: Throwable) {
                 Log.e("MainViewModel", "onFailure : ${t.message}")
             }
-
         })
     }
 
-    fun getSigns(): LiveData<List<SignItem?>?> {
-        return listSigns
+    fun getNews(): LiveData<List<NewsDataItem>> {
+        return listNews
     }
 }
