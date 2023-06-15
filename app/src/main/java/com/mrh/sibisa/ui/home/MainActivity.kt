0c3dbb16911aca3e60e87cc02dad3898d3f4b691
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-06-15 18:18:06 +0700

    added method to show custom dialog prompt when logout button press

--- app/src/main/java/com/mrh/sibisa/ui/home/MainActivity.kt
+++ app/src/main/java/com/mrh/sibisa/ui/home/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.mrh.sibisa.ui.home
 
 import android.Manifest
+import android.app.Dialog
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.Window
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.mrh.sibisa.ui.home.fragment.HomeFragment
 import com.mrh.sibisa.R
 import com.mrh.sibisa.databinding.ActivityMainBinding
@@ -55,12 +63,11 @@ class MainActivity : AppCompatActivity() {
         authViewModel = AuthViewModel()
         binding.bottomNavigation.background = null
         binding.bottomNavigation.menu.getItem(2).isEnabled = false
-        val sharedPreferences = getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
-        val editorPref = sharedPreferences.edit()
+        val sharedPreferences: SharedPreferences = getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
         val token = sharedPreferences.getString("AUTH_TOKEN", null)
-        Toast.makeText(this,token.toString(), Toast.LENGTH_SHORT).show()
+        switchLoginLogoutMenu(token)
 
-        val homeFragment = HomeFragment()
+        val homeFragment = HomeFragment(this)
         val profileFragment = ProfileFragment()
         val settingFragment = SettingFragment()
 
@@ -85,17 +92,9 @@ class MainActivity : AppCompatActivity() {
                 R.id.nav_setting -> replaceFragment(settingFragment)
                 R.id.nav_login -> {
                     if(token != null) {
-                        showLoading(true)
-                        authViewModel.logout(token)
-                        authViewModel.getLogoutResponse().observe(this) { response ->
-                            editorPref.apply {
-                                putString("AUTH_TOKEN", null)
-                            }.apply()
-                            Toast.makeText(this, response?.message, Toast.LENGTH_SHORT).show()
-                            startActivity(Intent(this, LoginActivity::class.java))
-                            finish()
-                            showLoading(false)
-                        }
+                        val promptTitle : String? = "Logout"
+                        val message :String? = "Apakah anda yakin ingin logout?"
+                        showCustomDialog(promptTitle, message, token, sharedPreferences)
                     } else {
                         startActivity(Intent(this, LoginActivity::class.java))
                         finish()
@@ -104,6 +103,54 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
+    }
+
+    private fun switchLoginLogoutMenu(token: String?) {
+        if(token != null) {
+            val navView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+            val authButton = navView.menu.findItem(R.id.nav_login)
+            authButton.title = "Logout"
+        }
+    }
+
+    private fun showCustomDialog(promptTitle: String?, message: String?, token: String, sharedPreferences: SharedPreferences) {
+        val editorPref = sharedPreferences.edit()
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.custom_dialog_alert)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val tvPromptTitle: TextView = dialog.findViewById(R.id.tv_prompt_title)
+        val tvPromptText: TextView = dialog.findViewById(R.id.tv_prompt_text)
+        tvPromptTitle.text = promptTitle
+        tvPromptText.text = message
+
+        val btnYes : Button = dialog.findViewById(R.id.btnDialogYes)
+        val btnNo : Button = dialog.findViewById(R.id.btnDialogNo)
+
+        btnYes.setOnClickListener {
+            showLoading(true)
+            authViewModel.logout(token)
+            authViewModel.getLogoutResponse().observe(this) { response ->
+                editorPref.apply {
+                    putString("AUTH_TOKEN", null)
+                    putString("NAME", null)
+                    putString("EMAIL", null)
+                }.apply()
+                Toast.makeText(this, response?.message, Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, LoginActivity::class.java))
+                finish()
+                showLoading(false)
+                dialog.dismiss()
+            }
+        }
+
+        btnNo.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.show()
     }
 
     override fun onBackPressed() {
