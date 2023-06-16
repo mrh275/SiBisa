e0e72352750ed423c7d65697501aba4d52b2bed8
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-06-16 12:51:04 +0700

    added endpoint for update user data

--- app/src/main/java/com/mrh/sibisa/network/api/ApiService.kt
+++ app/src/main/java/com/mrh/sibisa/network/api/ApiService.kt
@@ -4,11 +4,13 @@ import com.mrh.sibisa.data.login.ResponseLogin
 import com.mrh.sibisa.data.logout.ResponseLogout
 import com.mrh.sibisa.data.news.ResponseNews
 import com.mrh.sibisa.data.register.ResponseRegister
+import com.mrh.sibisa.data.users.ResponseUserUpdate
 import retrofit2.Call
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface ApiService {
 
@@ -32,4 +34,12 @@ interface ApiService {
 
     @GET("news")
     fun getAllNews(): Call<ResponseNews>
+
+    @FormUrlEncoded
+    @POST("user/update")
+    fun updateUser(
+        @Field("name") name: String?,
+        @Field("email") email: String?,
+        @Field("password") password: String?
+    ) : Call<ResponseUserUpdate>
 }
