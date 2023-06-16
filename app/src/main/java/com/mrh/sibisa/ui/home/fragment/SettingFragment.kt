c201fe183fb38c1ea229503ace5e0ceaf34377cd
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-06-16 12:55:34 +0700

    added fragment pop up dialog for each menu item

--- app/src/main/java/com/mrh/sibisa/ui/home/fragment/SettingFragment.kt
+++ app/src/main/java/com/mrh/sibisa/ui/home/fragment/SettingFragment.kt
@@ -1,11 +1,8 @@
 package com.mrh.sibisa.ui.home.fragment
 
-import android.annotation.SuppressLint
-import android.app.Dialog
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -14,10 +11,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.mrh.sibisa.R
+import com.mrh.sibisa.ui.auth.login.LoginActivity
 import com.mrh.sibisa.ui.customdialog.PopUpFragment
 
-class SettingFragment(context: Context) : Fragment() {
+class SettingFragment(context: Context, token: String) : Fragment() {
 
+    private val token = token
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -32,18 +31,41 @@ class SettingFragment(context: Context) : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val sharedPreferences = context?.getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
+        checkAuth(sharedPreferences)
         super.onViewCreated(view, savedInstanceState)
 
         val editName = view.findViewById<TextView>(R.id.settingChangeName)
+        val editEmail = view.findViewById<TextView>(R.id.settingChangeEmail)
+        val editPassword = view.findViewById<TextView>(R.id.settingChangePassword)
 
         editName.setOnClickListener {
             val promptTitle = editName.text.toString()
-            showDialogEdit(promptTitle)
+            showDialogEdit(promptTitle, 1, token)
+        }
+
+        editEmail.setOnClickListener {
+            val promptTitle = editEmail.text.toString()
+            showDialogEdit(promptTitle, 2, token)
+        }
+
+        editPassword.setOnClickListener {
+            val promptTitle = editPassword.text.toString()
+            showDialogEdit(promptTitle, 3, token)
         }
     }
 
-    private fun showDialogEdit(promptTitle: String) {
-        val showPopUp = PopUpFragment(promptTitle)
+    private fun checkAuth(sharedPreferences: SharedPreferences?) {
+        if(sharedPreferences?.getString("AUTH_TOKEN", null) == null) {
+            activity?.let {
+                it.startActivity(Intent(it, LoginActivity::class.java))
+                it.finishAfterTransition()
+            }
+        }
+    }
+
+    private fun showDialogEdit(promptTitle: String, idDialog: Int, token: String) {
+        val showPopUp = PopUpFragment(promptTitle, idDialog, token)
         showPopUp.show((activity as AppCompatActivity).supportFragmentManager, "showPopUp")
     }
 }
