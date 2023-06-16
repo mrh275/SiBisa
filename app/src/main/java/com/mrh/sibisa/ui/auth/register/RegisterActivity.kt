f0a285e0b2694cc22d2d2665649e5122aa218960
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-06-16 12:54:09 +0700

    added event for back press

--- app/src/main/java/com/mrh/sibisa/ui/auth/register/RegisterActivity.kt
+++ app/src/main/java/com/mrh/sibisa/ui/auth/register/RegisterActivity.kt
@@ -6,9 +6,6 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
 import com.mrh.sibisa.databinding.ActivityRegisterBinding
 import com.mrh.sibisa.ui.auth.AuthViewModel
 import com.mrh.sibisa.ui.auth.login.LoginActivity
@@ -47,6 +44,12 @@ class RegisterActivity : AppCompatActivity() {
                 Toast.makeText(this, "Registrasi gagal! Kendala server", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        startActivity(Intent(this, LoginActivity::class.java))
+        finishAfterTransition()
     }
 
     private fun showLoading(isLoading: Boolean) {
