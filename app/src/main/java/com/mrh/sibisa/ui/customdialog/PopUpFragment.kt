583ea643585d84380e80d0e7c9721bc884de58c0
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-06-16 12:53:31 +0700

    create method for update user data base on each prompt id

--- app/src/main/java/com/mrh/sibisa/ui/customdialog/PopUpFragment.kt
+++ app/src/main/java/com/mrh/sibisa/ui/customdialog/PopUpFragment.kt
@@ -9,13 +9,18 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import com.mrh.sibisa.R
+import com.mrh.sibisa.ui.home.MainViewModel
 
 
-class PopUpFragment(promptTitle: String) : DialogFragment() {
+class PopUpFragment(promptTitle: String, idDialog: Int, token: String) : DialogFragment() {
 
     private val promptTitle = promptTitle
+    private val idDialog = idDialog
+    private val token = token
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,22 +32,59 @@ class PopUpFragment(promptTitle: String) : DialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val sharedPreferences = context?.getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
+        val sharedPreferences = requireContext().getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
+        viewModel = MainViewModel()
 
         val btnYes = view.findViewById<Button>(R.id.btnDialogYes)
         val btnNo = view.findViewById<Button>(R.id.btnDialogNo)
         val promptTitleText = view.findViewById<TextView>(R.id.tv_prompt_title)
         val editText = view.findViewById<EditText>(R.id.edit_prompt_text)
 
+
         promptTitleText.text = promptTitle
-        editText.setText(sharedPreferences?.getString("NAME", null))
+
+        if(idDialog == 1) {
+            val oldValue = sharedPreferences?.getString("NAME", null)
+            editText.setText(oldValue)
+        } else if(idDialog == 2) {
+            val oldValue = sharedPreferences?.getString("EMAIL", null)
+            editText.setText(oldValue)
+        } else if(idDialog == 3) {
+            val oldValue = sharedPreferences?.getString("PASSWORD", null)
+            editText.setText(oldValue)
+        }
 
         btnNo.setOnClickListener {
             dismiss()
         }
 
         btnYes.setOnClickListener {
-            dismiss()
+            val editor = sharedPreferences.edit()
+            var newName = sharedPreferences?.getString("NAME", null)
+            var newEmail = sharedPreferences?.getString("EMAIL", null)
+            var newPassword = ""
+            when (idDialog) {
+                1 -> {
+                    newName = editText.text.toString()
+                    editor.apply{
+                        putString("NAME", newName)
+                    }.apply()
+                }
+                2 -> {
+                    newEmail = editText.text.toString()
+                    editor.apply{
+                        putString("EMAIL", newEmail)
+                    }.apply()
+                }
+                3 -> {
+                    newPassword = editText.text.toString()
+                }
+            }
+            viewModel.updateUser(newName, newEmail, newPassword, token)
+            viewModel.getUserUpdateResponse().observe(viewLifecycleOwner) {
+                Toast.makeText(requireContext(), it?.message, Toast.LENGTH_SHORT).show()
+                dismiss()
+            }
         }
     }
 }
