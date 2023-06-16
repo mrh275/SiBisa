cea77656ead657d6f44d13b316f88237c6ad9f37
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-06-16 12:53:55 +0700

    added auth checking

--- app/src/main/java/com/mrh/sibisa/ui/home/fragment/ProfileFragment.kt
+++ app/src/main/java/com/mrh/sibisa/ui/home/fragment/ProfileFragment.kt
@@ -1,6 +1,8 @@
 package com.mrh.sibisa.ui.home.fragment
 
 import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import com.mrh.sibisa.R
+import com.mrh.sibisa.ui.auth.login.LoginActivity
 
 class ProfileFragment : Fragment() {
 
@@ -24,12 +27,22 @@ class ProfileFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
         val sharedPreferences = context?.getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
+        checkAuth(sharedPreferences)
+        super.onViewCreated(view, savedInstanceState)
 
         val profileName = view.findViewById<TextView>(R.id.tv_profile_name)
         val profileEmail = view.findViewById<TextView>(R.id.tv_profile_email)
         profileName.text = sharedPreferences?.getString("NAME", "Nama Pengguna")
         profileEmail.text = sharedPreferences?.getString("EMAIL", "email@email.com")
+    }
+
+    private fun checkAuth(sharedPreferences: SharedPreferences?) {
+        if(sharedPreferences?.getString("AUTH_TOKEN", null) == null) {
+            activity?.let {
+                it.startActivity(Intent(it, LoginActivity::class.java))
+                it.finishAfterTransition()
+            }
+        }
     }
 }
